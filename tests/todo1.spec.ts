dfaa8edbc8b13df2961638a3850720a0c2d0f3f2
linhphan91 <yurimi.phan@gmail.com>
2026-06-19 09:33:04 +0700

    home exercises/ts-todo-app

--- tests/todo1.spec.ts
+++ tests/todo1.spec.ts
@@ -4,10 +4,127 @@ Built-in locators in Playwright are designed to be resilient and easy to use. Th
 
 */
 
-test('test', async ({ page }) => {
+test('Verify adding new todo successfully', async ({ page }) => {
   await page.goto('https://todomvc.com/examples/react/dist/');
-  await page.getByRole('textbox', {name: 'New Todo Input'}).fill('Buy milk');
-  await page.getByRole('textbox', {name: 'New Todo Input'}).press('Enter');
-  
+  await page.getByPlaceholder('What needs to be done?').fill('Buy milk'); //Get locator by Attribute
+  await page.getByPlaceholder('What needs to be done?').press('Enter');
+
+  await page.getByRole('textbox', { name: 'New Todo Input' }).fill('Buy bread'); //Get locator from DOM Tree --> Accessbility Tree
+  await page.getByRole('textbox', { name: 'New Todo Input' }).press('Enter');
+
+  await page.locator('.new-todo').fill('Buy eggs'); //Get locator by CSS/Xpath
+  await page.locator('.new-todo').press('Enter');
+
+  expect(await page.getByTestId('todo-item-label')
+    .count()).toBe(3);
+
+  await expect(page.getByTestId('todo-item-label'))
+    .toHaveText(['Buy milk', 'Buy bread', 'Buy eggs']); //trả về array
+
+
 
 });
+
+test('Verify marking complete todo successfully', async ({ page }) => {
+  await page.goto('https://todomvc.com/examples/react/dist/');
+  await page.getByPlaceholder('What needs to be done?').fill('Buy milk'); //Get locator by Attribute
+  await page.getByPlaceholder('What needs to be done?').press('Enter');
+
+  await page.getByRole('textbox', { name: 'New Todo Input' }).fill('Buy bread'); //Get locator from DOM Tree --> Accessbility Tree
+  await page.getByRole('textbox', { name: 'New Todo Input' }).press('Enter');
+
+  await page.locator('.new-todo').fill('Buy eggs'); //Get locator by CSS/Xpath
+  await page.locator('.new-todo').press('Enter');
+  await page
+    .getByRole('listitem') //return all
+    .filter({ hasText: 'Buy milk' })
+    .getByTestId('todo-item-toggle')
+    .click();
+
+  expect(await page
+    .getByRole('listitem') //return all
+    .filter({ hasText: 'Buy milk' })
+    .getByTestId('todo-item-toggle')
+    .isChecked())
+    .toBeTruthy();
+});
+
+
+test('Verify deleting todo successfully', async ({ page }) => {
+  await page.goto('https://todomvc.com/examples/react/dist/');
+  await page.getByPlaceholder('What needs to be done?').fill('Buy milk'); //Get locator by Attribute
+  await page.getByPlaceholder('What needs to be done?').press('Enter');
+
+  await page.getByRole('textbox', { name: 'New Todo Input' }).fill('Buy bread'); //Get locator from DOM Tree --> Accessbility Tree
+  await page.getByRole('textbox', { name: 'New Todo Input' }).press('Enter');
+
+  await page.locator('.new-todo').fill('Buy eggs'); //Get locator by CSS/Xpath
+  await page.locator('.new-todo').press('Enter');
+
+  await page
+    .getByRole('listitem') //return all
+    .filter({ hasText: 'Buy milk' })
+    .hover();
+
+  await page
+    .getByRole('listitem') //return all
+    .filter({ hasText: 'Buy milk' })
+    .getByRole('button', { name: 'Delete Todo' })
+    .click();
+
+  expect(await page
+    .getByRole('listitem') //return all
+    .filter({ hasText: 'Buy milk' })
+    .count())
+    .toBe(0);
+});
+
+
+test('Verify renaming todo successfully', async ({ page }) => {
+  await page.goto('https://todomvc.com/examples/react/dist/');
+  await page.getByPlaceholder('What needs to be done?').fill('Buy milk'); //Get locator by Attribute
+  await page.getByPlaceholder('What needs to be done?').press('Enter');
+
+  await page.getByRole('textbox', { name: 'New Todo Input' }).fill('Buy bread'); //Get locator from DOM Tree --> Accessbility Tree
+  await page.getByRole('textbox', { name: 'New Todo Input' }).press('Enter');
+
+  await page.locator('.new-todo').fill('Buy eggs'); //Get locator by CSS/Xpath
+  await page.locator('.new-todo').press('Enter');
+
+  await page
+    .getByRole('listitem') //return all
+    .filter({ hasText: 'Buy milk' })
+    .dblclick();
+    
+  await page
+    .getByTestId('todo-list')
+    .getByTestId('text-input')
+    .press('Control+A');
+
+  await page
+    .getByTestId('todo-list')
+    .getByTestId('text-input')
+    .fill('Buy almond milk');
+
+  await page
+    .getByTestId('todo-list')
+    .getByTestId('text-input')
+    .press('Enter');
+
+  expect(await page
+    .getByRole('listitem') //return all
+    .filter({ hasText: 'Buy almond milk' })
+    .count())
+    .toBe(1);
+    
+     expect(await page
+    .getByRole('listitem') //return all
+    .filter({ hasText: 'Buy milk' })
+    .count())
+    .toBe(0);
+
+});
+
+function press(arg0: string) {
+  throw new Error('Function not implemented.');
+}
